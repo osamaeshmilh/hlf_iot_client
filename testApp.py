20b09f0374a85e4fb22331aa5173572cde99c360
osamaeshmilh <osamaeshmilh@gmail.com>
2023-08-10 20:18:49 +0100

    testing

--- testApp.py
+++ testApp.py
@@ -1,79 +1,72 @@
-import paho.mqtt.client as mqtt
+import asyncio
+from gmqtt import Client as MQTTClient
 from sense_hat import SenseHat
 import json
 import datetime
-import time
 
-# Offline queue to hold messages when disconnected
-offline_queue = []
+BROKER_HOST = "149.102.154.14"
+BROKER_PORT = 1883
+USERNAME = "iot"
+PASSWORD = "iot123456"
+
+STOP = asyncio.Event()
+
+def on_connect(client, flags, rc, properties):
+    print("Connected")
+
+def on_disconnect(client, packet, exc=None):
+    print("Disconnected")
+
+def on_message(client, topic, payload, qos, properties):
+    print(topic, payload)
+
+def ask_exit(*args):
+    STOP.set()
 
 # Function to get sensor data
-def get_sensor_data(device_id, sensor_id):
+async def get_sensor_data(device_id, sensor_id):
     sense = SenseHat()
     temp = sense.get_temperature()
     humidity = sense.get_humidity()
     data = {
         'batchNo': '101',
         'warehouseNo': '01',
-        'iotId': f'{device_id:02}',  # Format it to be 01, 02, 03, ...
-        'temperatureSensorId': f'{sensor_id:02}',  # Format it to be 01, 02, 03, ...
+        'iotId': f'{device_id:02}',
+        'temperatureSensorId': f'{sensor_id:02}',
         'humiditySensorId': f'{sensor_id:02}',
         'timestamp': datetime.datetime.now().isoformat(),
         'temperature': str(temp),
         'humidity': str(humidity)
     }
-    print("Sensor data: ", data)
     return json.dumps(data)
 
-def on_connect(client, userdata, flags, rc):
-    global offline_queue
-    print(f"Connected with result code {str(rc)}")
-    # Publish any messages that were queued while offline
-    for message in offline_queue:
-        client.publish("iot/data", message)
-    offline_queue = []
+async def send_data(device_id, sensor_id, client):
+    while True:
+        sensor_data = await get_sensor_data(device_id, sensor_id)
+        print(f"Publishing data from device {device_id} and sensor {sensor_id}")
+        client.publish("iot/data", sensor_data)
+        await asyncio.sleep(1)
 
-# Callback for when the client is disconnected from the server.
-def on_disconnect(client, userdata, rc):
-    print(f"Disconnected with result code {str(rc)}")
+async def main():
+    client = MQTTClient("client-id")
 
-# Callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+    client.set_auth_credentials(USERNAME, PASSWORD)
+    await client.connect(BROKER_HOST, BROKER_PORT)
 
-# Setup MQTT client
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
+    tasks = []
+    for device_id in range(1, 11):
+        for sensor_id in range(1, 6):
+            task = asyncio.create_task(send_data(device_id, sensor_id, client))
+            tasks.append(task)
 
-# Set the MQTT broker IP address and port
-broker_ip = "149.102.154.14"
-broker_port = 1883
+    await STOP.wait()
+    for task in tasks:
+        task.cancel()
+    await client.disconnect()
 
-print(f"Connecting to broker at {broker_ip}:{broker_port}")
-# Connect to the broker
-client.username_pw_set("iot", "iot123456")
-client.connect(broker_ip, broker_port, 60)
-
-# Start the MQTT client
-client.loop_start()
-
-while True:
-    for device_id in range(1, 11):  # Looping for 10 devices
-        for sensor_id in range(1, 6):  # Looping for 5 sensors for each device
-            print(f"Getting sensor data for device {device_id} and sensor {sensor_id}")
-            # Get the sensor data
-            sensor_data = get_sensor_data(device_id, sensor_id)
-
-            print("Publishing sensor data to topic iot/data")
-            # Check if client is connected to the broker
-            if client.is_connected():
-                # Publish the sensor data to a topic
-                client.publish("iot/data", sensor_data)
-            else:
-                print("Offline. Adding to queue.")
-                offline_queue.append(sensor_data)
-
-            # Sleep for 5 seconds before next iteration
-            time.sleep(5)
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
