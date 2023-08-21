e3d09707ed7aa89e2ca2c8873a48f1cbed16afa4
osamaeshmilh <osamaeshmilh@gmail.com>
2023-08-21 13:31:02 +0100

    testing

--- testAppSingle.py
+++ testAppSingle.py
@@ -12,24 +12,28 @@ PASSWORD = "iot123456"
 STOP = asyncio.Event()
 
 # Global variables for temperature and humidity
-global_temp = None
-global_humidity = None
+global_temp = 0.0
+global_humidity = 0.0
 
-sense = SenseHat()  # Initialize it once instead of multiple times
+sense = SenseHat()
+
 
 def on_connect(client, flags, rc, properties):
     print("Connected")
 
+
 def on_disconnect(client, packet, exc=None):
     print("Disconnected")
+
 
 def on_message(client, topic, payload, qos, properties):
     print(topic, payload)
 
+
 def ask_exit(*args):
     STOP.set()
 
-# Asynchronous function to update temperature and humidity every two seconds
+
 async def update_sensor_values():
     global global_temp, global_humidity
     while True:
@@ -37,7 +41,7 @@ async def update_sensor_values():
         global_humidity = sense.get_humidity()
         await asyncio.sleep(2)
 
-# Function to get sensor data
+
 async def get_sensor_data(device_id, sensor_id):
     data = {
         'batchNo': '101',
@@ -49,14 +53,13 @@ async def get_sensor_data(device_id, sensor_id):
         'temperature': str(global_temp),
         'humidity': str(global_humidity)
     }
-    return json.dumps(data)
+    return data
 
-async def send_data(device_id, sensor_id, client):
-    while True:
-        sensor_data = await get_sensor_data(device_id, sensor_id)
-        print(f"Publishing data from device {device_id} and sensor {sensor_id}")
-        client.publish("iot/data", sensor_data)
-        await asyncio.sleep(1)
+
+async def send_bulk_data(client, bulk_data):
+    print("Publishing bulk data...")
+    client.publish("iot/data", json.dumps(bulk_data))
+
 
 async def main():
     client = MQTTClient("client-id")
@@ -67,19 +70,22 @@ async def main():
     client.set_auth_credentials(USERNAME, PASSWORD)
     await client.connect(BROKER_HOST, BROKER_PORT)
 
-    # Start the sensor value update task
     update_task = asyncio.create_task(update_sensor_values())
+    bulk_data = []
 
-    tasks = [update_task]
-    for device_id in range(1, 101):
-        for sensor_id in range(1, 2):
-            task = asyncio.create_task(send_data(device_id, sensor_id, client))
-            tasks.append(task)
+    for device_id in range(1, 1001):
+        for sensor_id in range(1, 6):
+            sensor_data = await get_sensor_data(device_id, sensor_id)
+            bulk_data.append(sensor_data)
+
+            if len(bulk_data) == 200:
+                await send_bulk_data(client, bulk_data)
+                bulk_data.clear()  # Clear the list for the next set of 3000
 
     await STOP.wait()
-    for task in tasks:
-        task.cancel()
+    update_task.cancel()
     await client.disconnect()
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
